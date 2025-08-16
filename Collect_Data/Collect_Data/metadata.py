8600357ae85535196a9d84f02df5ceae8b9f3381
Long Pham Ba <longphamba2k5@gmail.com>
2025-08-16 11:06:04 +0700

    Cập nhật nội dung week 3-4 collect data bổ sung

--- Collect_Data/Collect_Data/metadata.py
+++ Collect_Data/Collect_Data/metadata.py
@@ -8,16 +8,18 @@ def rebuild_metadata(output_dir, meta_path):
         try:
             data_npz = np.load(path, allow_pickle=True)
             commands = data_npz["steps/language_instruction"]
+            success = bool(data_npz["episode_metadata/success"])
             all_meta.append({
                 "episode": int(fname.split("_")[1].split(".")[0]),
                 "command": str(commands[0]) if len(commands) > 0 else "",
                 "steps": len(commands),
-                "file": path
+                "file": path,
+                "success": success
             })
         except Exception as e:
-            #print(f"Lỗi khi đọc {fname}: {e}")
-            pass
+            print(f"Error reading {fname}: {e}")
 
     with open(meta_path, "w", encoding="utf-8") as f:
         json.dump(all_meta, f, indent=2, ensure_ascii=False)
-    #print(f"📄 Đã cập nhật {meta_path} với {len(all_meta)} tập")
+    print(f"📄 Updated {meta_path} with {len(all_meta)} episodes")
+
